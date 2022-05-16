f94300b5b1a3341f8000a88efb0701488d153d8d
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2022-05-16 11:32:25 +1000

    install Express
    setup basic express server
    created route file for goal

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello from server!!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
