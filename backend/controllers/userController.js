4aa9b6e282e694c0ce991a8265b3e52d0f415a89
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2022-05-16 23:20:33 +1000

    User Routes and Controller Functions
    User

---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+// @desc Register new user
+// @route POST /api/users
+// @access Public
+const registerUser = (req, res) => {
+  res.json({ message: "Register User" });
+};
+// @desc Login user
+// @route POST /api/users/login
+// @access Public
+const loginUser = (req, res) => {
+  res.json({ message: "Login User" });
+};
+// @desc Get user data
+// @route GET /api/users/me
+// @access Public
+const getMe = (req, res) => {
+  res.json({ message: "User Data Display" });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
