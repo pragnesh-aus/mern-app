4c7b2ffa47f8ba4710e74bd46c018f181b4a30d7
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2022-05-16 11:46:00 +1000

    Update goalRoutes.js
    added routes for creating, read, updating and delete

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+// GET
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Get goals" });
+});
+// POST
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Set goal" });
+});
+// UPDATE
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+});
+// DELETE
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
 });
 
 module.exports = router;
