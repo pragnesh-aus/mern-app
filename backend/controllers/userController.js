d6681197983a6796f7a3707e39508eea4d4d12e7
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2022-05-17 00:23:04 +1000

    Generate JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,6 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -57,6 +58,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -72,6 +74,12 @@ const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User Data Display" });
 });
 
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 module.exports = {
   registerUser,
   loginUser,
