cac011804372c2a5cad0a9425baa24cdfd0411f7
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2022-05-23 11:27:20 +1000

    Hook up Login

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -11,6 +11,16 @@ const register = async (userData) => {
   return response.data;
 };
 
+//Login User
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
 // LOGOUT
 const logout = () => {
   localStorage.removeItem("user");
@@ -18,5 +28,6 @@ const logout = () => {
 const authService = {
   register,
   logout,
+  login,
 };
 export default authService;
